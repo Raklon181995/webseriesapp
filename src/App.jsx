d13742829f59727b9408a61fe1eb80f391ef1731
Rakesh Lonikar <rakeshlonikar@gmail.com>
2021-06-01 14:18:15 +0530

    Simeple Netflix app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Cards';
+import Sdata from './Sdata';
+ 
+
+
+const App = () => ( 
+  <>
+    <h1 className="heading_style">List Of  Netflix Series</h1>
+    {Sdata.map((val) => {
+        return(
+          <Card
+          key={val.id}
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+        );
+    })}
+</>
+);
+export default App; 
---
+++ src/Sdata.jsx
@@ -0,0 +1,24 @@
+const Sdata = [
+    {
+        id: "1",
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Orginal Series",
+        sname: "DARK",
+        link :"https://www.netflix.com/in/title/80998668?source=35",
+    },
+    {
+        id: "2",
+        imgsrc: "https://upload.wikimedia.org/wikipedia/en/5/52/Ghoul_-_2018_Poster.jpg",
+        title: "A Netflix Orginal Series",
+        sname: "Ghoul",
+        link: "https://www.netflix.com/in/title/80998668?source=35",
+    },
+    {
+        id: "3",
+        imgsrc: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title: "A Netflix Orginal Series",
+        sname: "Stranger Things",
+        link: "https://www.netflix.com/in/title/80998668?source=35",
+    },
+]
+export default Sdata;
